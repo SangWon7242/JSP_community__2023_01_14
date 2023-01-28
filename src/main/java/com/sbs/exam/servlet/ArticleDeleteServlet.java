ada1c5ee10b5370e947e6f025e8b89a3660e6e14
SangWon7242 <gotkdqja@gmail.com>
2023-01-28 10:25:47 +0900

    게시물 삭제하기 구현

---
+++ src/main/java/com/sbs/exam/servlet/ArticleDeleteServlet.java
@@ -0,0 +1,65 @@
+package com.sbs.exam.servlet;
+
+import com.sbs.exam.util.DBUtil;
+import com.sbs.exam.util.SecSql;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+@WebServlet("/article/doDelete")
+public class ArticleDeleteServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
+    resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
+    resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
+
+    // DB 연결시작
+    Connection conn = null;
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.printf("[ClassNotFoundException 예외, %s]", e.getMessage());
+      System.out.println("DB 드라이버 클래스 로딩 실패");
+      return;
+    }
+
+    String url = "jdbc:mysql://127.0.0.1:3306/JSP_Community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+    String user = "sbsst";
+    String password = "sbs123414";
+
+    try {
+      conn = DriverManager.getConnection(url, user, password);
+
+      int id = Integer.parseInt(req.getParameter("id"));
+
+      SecSql sql = SecSql.from("DELETE");
+      sql.append("FROM article");
+      sql.append("WHERE id = ?", id);
+
+      DBUtil.delete(conn, sql);
+      resp.getWriter().append("<script> alert('1번 글이 삭제되었습니다.'); location.replace('list'); </script>");
+
+    } catch (SQLException e) {
+     e.printStackTrace();
+    } finally {
+      try {
+        if (conn != null && !conn.isClosed()) {
+          conn.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+    // DB 연결 끝
+  }
+}
