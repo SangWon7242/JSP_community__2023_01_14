80c608b86cb89c8f28961437714c7a8f850d6899
SangWon7242 <gotkdqja@gmail.com>
2023-02-05 09:46:30 +0900

    게시물 작성시 로그인 된 회원의 번호가 article 테이블에 같이 저장 되도록 작업

--- src/main/java/com/sbs/exam/servlet/ArticleDoWriteServlet.java
+++ src/main/java/com/sbs/exam/servlet/ArticleDoWriteServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -27,6 +28,12 @@ public class ArticleDoWriteServlet extends HttpServlet {
 
     String diverName = Config.getDriverClassName();
 
+    HttpSession session = req.getSession();
+
+    if(session.getAttribute("loginedMemberId") == null) {
+      rq.appendBody(String.format("<script> alert('로그인 후 이용해주세요.'); location.replace('../member/login'); </script>"));
+    }
+
     try {
       Class.forName(diverName);
     } catch (ClassNotFoundException e) {
@@ -41,11 +48,14 @@ public class ArticleDoWriteServlet extends HttpServlet {
       String title = rq.getParam("title", "");
       String body = rq.getParam("body", "");
 
+      int loginedMemberId = (int) session.getAttribute("loginedMemberId");
+
       SecSql sql = SecSql.from("INSERT INTO article");
       sql.append("SET regDate = NOW()");
       sql.append(", updateDate = NOW()");
       sql.append(", title = ?" , title);
       sql.append(", body = ?" , body);
+      sql.append(", memberId = ?" , loginedMemberId);
 
       int id = DBUtil.insert(conn, sql);
       rq.appendBody(String.format("<script> alert('%d번 글이 생성되었습니다.'); location.replace('list'); </script>", id));
