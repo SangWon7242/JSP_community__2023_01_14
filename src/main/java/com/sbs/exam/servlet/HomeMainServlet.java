8ff2789adf300a807ca4a3d3a6ef3b2c3965925a
SangWon7242 <gotkdqja@gmail.com>
2023-02-05 09:23:13 +0900

    로그아웃 기능 구현

--- src/main/java/com/sbs/exam/servlet/HomeMainServlet.java
+++ src/main/java/com/sbs/exam/servlet/HomeMainServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -13,8 +14,20 @@ import java.io.IOException;
 public class HomeMainServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      Rq rq = new Rq(req, resp);
-      rq.jsp("../home/main");
+    Rq rq = new Rq(req, resp);
+
+    HttpSession session = req.getSession();
+    boolean isLogined = false;
+    int loginedMemberId = -1;
+
+    if( session.getAttribute("loginedMemberId") != null) {
+      loginedMemberId = (int) session.getAttribute("loginedMemberId");
+      isLogined = true;
+    }
+
+    req.setAttribute("isLogined", isLogined);
+    req.setAttribute("loginedMemberId", loginedMemberId);
+    rq.jsp("../home/main");
   }
 
   @Override
--- src/main/webapp/home/main.jsp
+++ src/main/webapp/home/main.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
+<%
+boolean isLogined = (boolean) request.getAttribute("isLogined");
+int loginedMemberId = (int) request.getAttribute("loginedMemberId");
+%>
+
 <!doctype html>
 <html lang="ko">
 <head>
@@ -8,6 +13,15 @@
 </head>
 <body>
 <h1>메인 페이지</h1>
+
+  <% if ( isLogined ) { %>
+    <%=loginedMemberId%>번 회원님 환영합니다.
+    <a href="../member/doLogout">로그아웃</a>
+  <% } %>
+
+  <% if ( !isLogined ) { %>
+    <a href="../member/login">로그인</a>
+  <% } %>
 
   <div>
     <a href="../article/list">게시물 리스트</a>
